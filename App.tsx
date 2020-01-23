35309d1847cc6b2ac6c908743b5b93c6b36ff726
Ruban <ragularuban@hotmail.com>
2020-01-23 07:00:06 +0530

    Initial Commit - Blank Hello World React Native Expo Project

---
+++ App.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { Component } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+
+
+export default class App extends Component<AppProps, AppState> {
+
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.h1}>Hello World</Text>
+      </View>
+    );
+  }
+}
+
+export interface AppProps {
+  counter: number;
+}
+
+export interface AppState {
+  counter: number;
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    paddingTop: 60,
+  },
+  h1: {
+    color: '#222',
+    fontFamily: 'Arial',
+    fontSize: 20,
+    textAlign: 'center',
+  }
+});
